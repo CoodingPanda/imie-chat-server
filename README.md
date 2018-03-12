b8cc6ff580f2fe65ece9fcda482d46213e8268f4
Gaëtan <gaetan.bodineau@hotmail.fr>
2018-03-12 13:53:12 +0100

    Tuto

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # imie-chat-server
+
+Pour utiliser le websocket il faut installer Gradle (il faut également avoir Java JDK ou JRE de version 7 ou supérieur d'installer)
+
+(Commande à entrer dans le GitBash)
+
+--> java -version
+
+Pour installer Gradle
+
+https://gradle.org/install/
+
+Télécharger le "Binary-only"
+Créer un dossier Gradle dans le C:/
+Extraire le dossier ZIP de gradle-4.6 dedans
+Ensuite clic-droit sur "Ce PC" -> "Propiétés" -> "Paramètres système avancés" -> "Variables d'environnement"
+Dans "Variables système", aller sur le "Path" et "Modifier" (une fenêtre "Modifier la variable d'environnement"
+"Nouveau" et entrer le chemin du Gradle (Par exemple: C:\Gradle\gradle-4.6\bin)
+
+Pour vérifier si Gradle est bien installer aller sur le GitBash et entrer gradle -v
+
+Une fois ceci fait, dans le GitBash aller dans le dossier "websocket" avec les commandes cd
+
+Une fois dedans entrer la commande ./gradlew tasks
+Puis ./gradlew build
+Et pour lancer le progamme entrer la commande gradle run
+
+Pour envoyer un message, aller sur une page internet, ouvrir l'inspecteur d'élément, aller dans console
+
+var server = new WebSocket("ws://localhost:8083"); //Permet de créer un nouveau WebSocket
+server.send("Le message");
+
+Et aller dans le GitBash pour voir le résultat
---
+++ websocket/src/main/java/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.websocket.DeploymentException;
+
+import fr.imie.chat.specification.WebSocketServer;
+import fr.imie.chat.specification.listeners.CloseWebSocketListener;
+import fr.imie.chat.specification.listeners.MessageWebSocketListener;
+import fr.imie.chat.specification.listeners.OpenWebSocketListener;
+
+public class Main {
+	public static void main(String[] args) throws DeploymentException, IOException {
+		new Main();
+	}
+
+
+	private Main() throws DeploymentException, IOException {
+		WebSocketServer<String> webSocketServer = WebSocketServer.get("localhost", 8083, String.class);
+		
+		webSocketServer.addListener(new OpenWebSocketListener<String>() {
+				@Override
+				public void onOpen(String sessionId) {
+					System.out.println("Open " + sessionId);
+				}
+		});
+		
+		webSocketServer.addListener(new CloseWebSocketListener<String>() {
+				@Override
+				public void onClose(String sessionId) {
+					System.out.println("Close " + sessionId);
+				}
+		});
+		
+		webSocketServer.addListener(new MessageWebSocketListener<String>() {
+				@Override
+				public void onMessage(String sessionId, String message) {
+					System.out.println("Message de " + sessionId);
+					System.out.println(message);
+				}
+		});
+		
+		webSocketServer.start();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Appuyez sur n'importe quelle touche pour arreter le serveur...");
+		reader.readLine();
+	}
+}
