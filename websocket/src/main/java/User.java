6a8cf3e49a7c571998b83ea39ef94fcf59179522
PeredaPy <pypereda@gmail.com>
2018-03-21 16:01:05 +0100

    authenOK

---
+++ websocket/src/main/java/User.java
@@ -0,0 +1,41 @@
+package Actions;
+
+public class User {
+
+    private String Email;
+    private String Password;
+    private int idUser;
+    private String Username;
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+}
